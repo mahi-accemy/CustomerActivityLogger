59f0f3be507bf12736e95d9cf8c5a8658c8b2225
Abdullah <abdullah@accemy.com>
2019-02-14 17:53:43 +0530

    SSL removed from Production URL

--- mahindralogger/src/main/java/com/accemy/mahindralogger/MahindraLoggerService.java
+++ mahindralogger/src/main/java/com/accemy/mahindralogger/MahindraLoggerService.java
@@ -19,7 +19,7 @@ import retrofit2.http.POST;
 public interface MahindraLoggerService {
 
     String UAT_ENDPOINT = "http://mapi.sitsyouv.com/"; //Test Environment
-    String PROD_ENDPOINT = "https://mapi.mahindrasyouv.com/"; //Live Environment
+    String PROD_ENDPOINT = "http://mapi.mahindrasyouv.com/"; //Live Environment
 
 //    @Headers({
 //            "x-functions-key: J3C4/9R8RGQzBYApFH6hm1VyUZ7M7XUhjzanFBpbaGDB1YAUyHXl0A==", //Test Environment
